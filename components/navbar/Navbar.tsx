0a006211ed051749edda91e9992ae94771bb75b9
UlisesHdz <ulisesdejesusk9999@gmail.com>
2025-03-27 18:00:23 -0600

    Next / Link

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,21 +1,47 @@
+import { HomeIcon } from "@primer/octicons-react";
+import Link from "next/link";
+
+const navItems = [
+  { path: "/about", text: "About" },
+  { path: "/pricing", text: "Pricing" },
+  { path: "/contact", text: "Contact" },
+];
+
 export const Navbar = () => {
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      <span>Home</span>
+      <Link href={"/"} className="flex items-center">
+        <HomeIcon className="mr-2" />
+        <span>Home</span>
+      </Link>
 
       {/* Espaciado flexible entre elementos */}
       <div className="flex flex-1"></div>
 
+
       {/* Enlaces con clase para el estilo */}
-      <a href="/about" className="ml-2 text-white">
+
+      {
+      navItems.map((item) => (
+        <Link key={item.path} href={item.path} className="ml-2 text-white">
+          {item.text}
+        </Link>
+      ))
+      }
+
+      {/*   
+          
+      <Link href="/about" className="ml-2 text-white">
         About
-      </a>
-      <a href="/contact" className="ml-2 text-white">
+      </Link>
+      <Link href="/contact" className="ml-2 text-white">
         Contact
-      </a>
-      <a href="/pricing" className="ml-2 text-white">
+      </Link>
+      <Link href="/pricing" className="ml-2 text-white">
         Pricing
-      </a>
+      </Link> 
+      */}
+
     </nav>
   );
 };
